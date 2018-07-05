88670d4f577b0cfe1e01ef9fc53295bc8b9d33ce
Димитър Арнаудов <dimitararnaudov68@gmail.com>
2018-07-05 16:52:01 +0300

    Added basic UI prototype and architecture prototype

---
+++ Colloseum.Gladiators/GladiatorStats.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colloseum.Gladiators
+{
+    class Stats
+    {
+        public int Hp { get; set; }
+        public int Atk { get; set; }
+        public int Def { get; set; }
+        public int Ctd { get; set; }//Chance to Dodge
+        public int Special { get; set; }
+    }
+}
---
+++ Colosseum/FightStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colosseum
+{
+    class FightStartup
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Colosseum where you play as a Lanist in ancient Rome.");
+            Console.WriteLine("Your role is to train gladiators and make them fight to the dead");
+            Console.WriteLine("Y");
+            Console.WriteLine("Choose your gladiators and let the battle start.");
+            Console.WriteLine("--------------------------------------------------------------------------");
+            Console.WriteLine("Its time to choose your warriors, train them and make them engage in battle to the dead");
+
+        }
+    }
+}
