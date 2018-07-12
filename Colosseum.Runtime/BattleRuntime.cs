b972c2043e8ef26f5bcd077838d341bd2cd5740c
Димитър Арнаудов <dimitararnaudov68@gmail.com>
2018-07-12 23:42:29 +0300

    Creted prototype for the runtime, UI, Code-Structure

---
+++ Colosseum.Runtime/BattleRuntime.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.ComponentModel.Design.Serialization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Colosseum.Gladiators;
+
+namespace Colosseum.Runtime
+{
+    public class BattleRuntime
+    {
+        //Selects Gladiator By Key Input
+        public static GladiatorStats SelectGladiatorByKeyInput(ConsoleKey key)
+        {
+            var gladiatorBase = Gladiators.Gladiators.BaseGladiators();
+
+            var gladiator = (from g in gladiatorBase
+                             where g.Key == key
+                             select g).FirstOrDefault();
+            
+            
+            return gladiator;
+        }
+
+        //Begins Fight Simulation
+        public static void BeginFight(GladiatorStats firstGladiator, GladiatorStats secondGladiator)
+        {
+            int firstGladiatorDamge = firstGladiator.Atk - secondGladiator.Def;
+            int firstGladiatorHp = firstGladiator.Hp;
+
+            int secondGladiatorHp = secondGladiator.Hp;
+            int secondGladiatorDamge = secondGladiator.Atk - firstGladiator.Def;
+
+            while (firstGladiatorHp > 0)
+            {
+                secondGladiatorHp -= firstGladiatorDamge;
+
+                if (secondGladiatorHp == 0)
+                {
+                    break;
+                }
+
+                while (secondGladiatorHp > 0)
+                {
+                    firstGladiatorHp -= secondGladiatorDamge;
+                    break;
+                }
+            }
+
+            if (firstGladiatorHp == 0)
+            {
+                Console.WriteLine(secondGladiator.Name + " Wins");
+            }
+            else
+            {
+                Console.WriteLine(firstGladiator.Name + " Wins");
+            }
+        }
+    }
+}
---
+++ Colosseum/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Colosseum.Runtime;
+
+namespace Colosseum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Main UI
+            Console.WriteLine("!!!THIS IS A VERY EARLY BUILD OF COLOSSEUM SO EXPECT BUGS!!!");
+            Console.WriteLine("!!!FOR NOW I HAVE IMPLEMENTED ONLY TWO GLADIATORS MORE TO COME IN THE FUTURE!!!");
+            Console.WriteLine("!!!THE OPTION THE CREATE YOUR OWN GLADIATORS IS STILL IN WORK SO PLEASE BE PATIENT!!!");
+            Console.WriteLine("--------------------------------------------------------------------------");
+            Console.WriteLine("Welcome to Colosseum where you play as a Lanist in ancient Rome.");
+            Console.WriteLine("Choose your warriors, train them and make them engage in battle to the dead");
+            Console.WriteLine("--------------------------------------------------------------------------");
+            Console.WriteLine("Retarius --> press R on the keyboard to pick this gladiator");
+            Console.WriteLine("Myrmillonis --> press M on the keyboard to pick this gladiator");
+
+            //UI for First Gladiator
+            var firstKeyInput = Console.ReadKey(true);
+
+            var firstGladiator = BattleRuntime.SelectGladiatorByKeyInput(firstKeyInput.Key);
+            
+            Console.WriteLine("You have picked" + firstGladiator.Name);
+            
+            //UI for Second Gladiator
+            var secondKeyInput = Console.ReadKey(true);
+
+            var secondGladiator = BattleRuntime.SelectGladiatorByKeyInput(secondKeyInput.Key);
+
+            Console.WriteLine("You have picked" + secondGladiator.Name);
+            
+            //Battle Start-up
+            BattleRuntime.BeginFight(firstGladiator, secondGladiator);
+
+            Console.ReadKey();
+        }
+    }
+}
